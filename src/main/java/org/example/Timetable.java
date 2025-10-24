8015595057d49dbdbe30bc5e763352b413de6c71
garupf <maestro-tmn@yandex.ru>
2025-10-24 22:12:44 +0500

    Initial commit

---
+++ src/main/java/org/example/Timetable.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Timetable {
+
+    //private /* как это хранить??? */ timetable
+
+    public void addNewTrainingSession(TrainingSession trainingSession) {
+        //сохраняем занятие в расписании
+    }
+
+    public TrainingSession getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+        return null;
+    }
+
+    public TrainingSession  getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+        return null;
+    }
+
+}
